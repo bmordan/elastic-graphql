2840f5212933a53df3a7c96261048567b9d5a4d6
bmordan <bernardmordan@gmail.com>
2018-11-12 21:09:02 +0000

    quick little setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema')
+
+const expressPort = process.env.port || process.env.PORT || 9201;
+
+const server = express();
+server.use(
+  '/',
+  graphqlHTTP({
+    schema: (schema),
+    graphiql: true,
+    formatError: error => ({
+      message: error.message,
+      stack: error.stack.split('\n'),
+    }),
+  })
+);
+
+server.listen(expressPort, () => {
+  console.log(`The server is running at http://localhost:${expressPort}/`);
+});
